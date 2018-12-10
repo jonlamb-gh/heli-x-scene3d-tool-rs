38de6867767d547822a34e668147d0dba52fef21
Jon Lamb <lamb.jon.io@gmail.com>
2018-12-10 14:05:23 -0800

    GPU mesh tiling, bug with joining the tiles

---
+++ src/heightmap.rs
@@ -0,0 +1,217 @@
+// TODO - support more than 8-bit depths
+
+use kiss3d::resource::{Mesh, MeshManager};
+use nalgebra::{normalize, Point3, Vector3};
+use png::{BitDepth, ColorType, Decoder, DecodingError};
+use std::cell::RefCell;
+use std::fs::File;
+use std::path::Path;
+use std::rc::Rc;
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum Error {
+    InvalidFile,
+    FileNotSupported,
+}
+
+#[derive(Debug)]
+pub struct Heightmap {
+    src_data: Vec<u8>,
+    width: usize,
+    height: usize,
+    height_scale: f32,
+    height_offset: f32,
+    tiles: Vec<Tile>,
+}
+
+const TILE_SIZE: usize = 128;
+
+#[derive(Debug)]
+pub struct Tile {
+    name: String,
+    start_x: usize,
+    start_y: usize,
+    width: usize,
+    height: usize,
+}
+
+impl Tile {
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+}
+
+impl Heightmap {
+    pub fn from_png_file<P: AsRef<Path>>(file_path: P) -> Result<Self, Error> {
+        let f = File::open(file_path)?;
+
+        let decoder = Decoder::new(f);
+        let (info, mut reader) = decoder.read_info().unwrap();
+
+        assert_eq!(
+            info.color_type,
+            ColorType::Grayscale,
+            "Only 8-bit grayscale is supported"
+        );
+
+        assert_eq!(
+            info.bit_depth,
+            BitDepth::Eight,
+            "Only 8-bit grayscale is supported"
+        );
+
+        // TODO - update these
+        // Enforce mesh tiling constraints
+        assert_eq!(
+            info.width as usize % TILE_SIZE,
+            0,
+            "Only mod {} dimensions are supported",
+            TILE_SIZE
+        );
+        assert_eq!(
+            info.height as usize % TILE_SIZE,
+            0,
+            "Only mod {} dimensions are supported",
+            TILE_SIZE
+        );
+
+        // Allocate and fill the output buffer
+        let mut buf: Vec<u8> = vec![0; info.buffer_size()];
+        reader.next_frame(&mut buf)?;
+
+        // Split up the grid into TILE_SIZE x TILE_SIZE meshes
+        let num_tiles_x = info.width as usize / TILE_SIZE;
+        let num_tiles_y = info.width as usize / TILE_SIZE;
+
+        let mut tiles: Vec<Tile> = Vec::new();
+        for ty in 0..num_tiles_y {
+            for tx in 0..num_tiles_x {
+                let tile = Tile {
+                    name: format!("{} {}", tx, ty),
+                    start_x: (tx * TILE_SIZE),
+                    start_y: (ty * TILE_SIZE),
+                    width: TILE_SIZE,
+                    height: TILE_SIZE,
+                };
+                tiles.push(tile);
+            }
+        }
+
+        Ok(Self {
+            src_data: buf,
+            width: info.width as _,
+            height: info.height as _,
+            height_scale: 5.0,
+            height_offset: 0.0,
+            tiles,
+        })
+    }
+
+    pub fn populate_mesh_manager(&self, mm: &mut MeshManager) -> &[Tile] {
+        for tile in &self.tiles {
+            let mesh = self.generate_mesh(tile);
+            //mm.add(Rc::new(RefCell::new(mesh)).clone(), tile.name());
+            mm.add(Rc::new(RefCell::new(mesh)), tile.name());
+        }
+
+        &self.tiles
+    }
+
+    fn generate_mesh(&self, tile: &Tile) -> Mesh {
+        let twidth = (self.width - 1) as f32;
+        let theight = (self.height - 1) as f32;
+        let half_twidth = twidth / 2_f32;
+        let half_theight = theight / 2_f32;
+
+        // For each quad in the mesh, generate two triangles
+        // Each triangle has three indices
+        let mut vertices: Vec<Point3<f32>> = vec![];
+        let mut normals: Vec<Vector3<f32>> = vec![];
+        let mut indices: Vec<Point3<u16>> = vec![];
+
+        // Generate the vertices for the vbo
+        for y in tile.start_y..(tile.start_y + tile.height) {
+            for x in tile.start_x..(tile.start_x + tile.width) {
+                let index = (y * self.width) + x;
+
+                let elevation: f32 = self.src_data[index] as f32 / 255.0;
+
+                let s = x as f32 / twidth;
+                let t = y as f32 / theight;
+
+                // align coordinate frame, Y-up
+                vertices.push(Point3::new(
+                    // image width mapped to X axis
+                    (s * twidth) - half_twidth,
+                    // depth/elevation mapped to Y axis
+                    (elevation * self.height_scale) + self.height_offset,
+                    // image height mapped to Y axis
+                    (t * theight) - half_theight,
+                ));
+
+                // Push empty normal vector
+                normals.push(Vector3::new(0_f32, 0_f32, 0_f32));
+            }
+        }
+
+        // Generate the indices for the ibo
+        for y in 0..(tile.height - 1) {
+            for x in 0..(tile.width - 1) {
+                let index = (y * tile.width) + x;
+
+                // top triangle T0 v0->v1->v2
+                indices.push(Point3::new(
+                    index as _,
+                    index as u16 + tile.width as u16 + 1,
+                    index as u16 + 1,
+                ));
+
+                // bottom triangle T1 v0->v1->v2
+                indices.push(Point3::new(
+                    index as _,
+                    index as u16 + tile.width as u16,
+                    index as u16 + tile.width as u16 + 1,
+                ));
+            }
+        }
+
+        // Generate the normals for the nbo
+        for ibo_idx in (0..indices.len()).step_by(3) {
+            let v0 = vertices[indices[ibo_idx].x as usize];
+            let v1 = vertices[indices[ibo_idx].y as usize];
+            let v2 = vertices[indices[ibo_idx].z as usize];
+
+            let a = v1 - v0;
+            let b = v2 - v0;
+
+            let normal = normalize(&a.cross(&b));
+
+            normals[indices[ibo_idx].x as usize] += normal;
+            normals[indices[ibo_idx].y as usize] += normal;
+            normals[indices[ibo_idx].z as usize] += normal;
+        }
+
+        // Normalize the normals
+        for n in normals.iter_mut() {
+            *n = normalize(n);
+        }
+
+        Mesh::new(vertices, indices, Some(normals), None, false)
+    }
+}
+
+impl From<std::io::Error> for Error {
+    fn from(e: std::io::Error) -> Error {
+        match e {
+            _ => Error::InvalidFile,
+        }
+    }
+}
+
+impl From<DecodingError> for Error {
+    fn from(e: DecodingError) -> Error {
+        match e {
+            _ => Error::FileNotSupported,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -7,96 +7,25 @@
 
 use kiss3d::camera::FirstPerson;
 use kiss3d::light::Light;
-use kiss3d::resource::Mesh;
+use kiss3d::resource::MeshManager;
 use kiss3d::window::Window;
 use nalgebra::{Point3, Translation3, Vector3};
-use std::cell::RefCell;
 use std::env;
-use std::fs::File;
-use std::rc::Rc;
+
+mod heightmap;
+
+use crate::heightmap::Heightmap;
 
 fn main() {
-    let source = match env::args().nth(1) {
-        Some(file_name) => File::open(file_name).unwrap(),
-        None => File::open("heightmap.png").unwrap(),
+    let file_path = if let Some(p) = env::args().nth(1) {
+        p
+    } else {
+        panic!("Expected a file path to heightmap.png");
     };
 
-    let decoder = png::Decoder::new(source);
-    let (info, mut reader) = decoder.read_info().unwrap();
-    assert_eq!(
-        info.color_type,
-        png::ColorType::Grayscale,
-        "Only grayscale is supported"
-    );
+    let hmap = Heightmap::from_png_file(&file_path).expect("Failed to create Heightmap");
 
-    println!("info.width {}", info.width);
-    println!("info.height {}", info.height);
-    println!("info.color_type {:#?}", info.color_type);
-    println!("info.bit_depth {:#?}", info.bit_depth);
-    println!("info.line_size {}", info.line_size);
-
-    // Allocate the output buffer.
-    let mut buf: Vec<u8> = vec![0; info.buffer_size()];
-
-    reader.next_frame(&mut buf).unwrap();
-
-    // f32/f64/GLfloat?
-    let mut coords: Vec<Point3<f32>> = vec![];
-
-    // GLuint?, kiss3d switched to u16 to support webgl,
-    // need to split into sub-meshes
-    let mut indices: Vec<Point3<u16>> = vec![];
-
-    let height_scale = 5_f32;
-    let height_offset = 0_f32;
-
-    let twidth = (info.width - 1) as f32;
-    let theight = (info.height - 1) as f32;
-    let half_twidth = twidth / 2_f32;
-    let half_theight = theight / 2_f32;
-
-    for y in 0..info.height {
-        for x in 0..info.width {
-            let index = ((y * info.width) + x) as usize;
-
-            let elevation: f32 = buf[index] as f32 / 255.0;
-
-            let s = x as f32 / twidth;
-            let t = y as f32 / theight;
-
-            coords.push(Point3::new(
-                (s * twidth) - half_twidth,
-                (t * theight) - half_theight,
-                (elevation * height_scale) + height_offset,
-            ));
-        }
-    }
-
-    for y in 0..(info.height - 1) {
-        for x in 0..(info.width - 1) {
-            let index = ((y * info.width) + x) as usize;
-
-            // top triangle T0 v0->v1->v2
-            indices.push(Point3::new(
-                index as _,
-                index as u16 + info.width as u16 + 1,
-                index as u16 + 1,
-            ));
-
-            // bottom triangle T1 v0->v1->v2
-            indices.push(Point3::new(
-                index as _,
-                index as u16 + info.width as u16,
-                index as u16 + info.width as u16 + 1,
-            ));
-        }
-    }
-
-    // TODO - normals?
-
-    let mesh = Rc::new(RefCell::new(Mesh::new(coords, indices, None, None, false)));
-
-    let eye = Point3::new(0.0, 0.0, 20.0);
+    let eye = Point3::new(0.0, 20.0, 0.0);
     let at = Point3::origin();
     let mut first_person_cam = FirstPerson::new(eye, at);
     //let mut first_person_cam = FirstPerson::new_with_frustrum(50.0, 1.0, 400.0,
@@ -106,25 +35,33 @@ fn main() {
     let mut window = Window::new("Heli-X Scene3D Tool");
     window.set_light(Light::StickToCamera);
 
+    let tiles = MeshManager::get_global_manager(|mm| hmap.populate_mesh_manager(mm));
+
     let mesh_scale = Vector3::new(1.0, 1.0, 1.0);
-    //let mesh_scale = Vector3::new(10.0, 10.0, 10.0);
-    let mut m = window.add_mesh(mesh, mesh_scale);
-    m.set_color(1.0, 0.0, 0.0);
-    //m.enable_backface_culling(false);
-    //m.set_surface_rendering_activation(true);
+    for tile in tiles {
+        let mut m = window
+            .add_geom_with_name(tile.name(), mesh_scale)
+            .expect("Failed to add mesh tile");
 
-    // Wireframe
-    m.set_surface_rendering_activation(false);
-    m.set_points_size(5.0);
-    m.set_lines_width(1.0);
+        // TODO - fix the tiling strip bug
+        //m.append_translation(&tile.alignment());
 
-    let mut c = window.add_cube(2.0, 2.0, 2.0);
+        m.set_color(1.0, 0.0, 0.0);
+        //m.enable_backface_culling(false);
+        //m.set_surface_rendering_activation(true);
+
+        // Wireframe
+        m.set_surface_rendering_activation(false);
+        m.set_points_size(5.0);
+        m.set_lines_width(1.0);
+    }
+
+    let mut c = window.add_cube(1.0, 1.0, 1.0);
     c.set_color(0.0, 0.0, 1.0);
 
-    let mut cb = window.add_cube(2.0, 2.0, 2.0);
+    let mut cb = window.add_cube(1.0, 1.0, 1.0);
     cb.set_color(0.0, 1.0, 1.0);
-    cb.append_translation(&Translation3::new(64.0, 0.0, 0.0));
+    cb.append_translation(&Translation3::new(128.0, 0.0, 0.0));
 
-    //while window.render() {}
     while window.render_with_camera(&mut first_person_cam) {}
 }
